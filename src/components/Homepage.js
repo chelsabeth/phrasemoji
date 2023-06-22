f8162ba3ef4e356b3df9508ae3c9319e36db6b51
Chelsea Wetzel <chelseawetzel@Bradleys-Air.localdomain>
2023-06-22 15:34:23 -0400

    Add all homepage properties

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,25 @@
+import './Homepage.css';
+
 function Homepage() {
     return (
-      <div className="App">
-        <h1>PHRASEMOJI</h1>
+      <div className="main-homepage">
+        <h1>✨ PHRASEMOJI ✨</h1>
+        <section>guess the phrase that matches the list of emojis <br/>
+            example: 😇 🐮 -> "Holy Cow"
+        </section>
+        <section>
+            {/* UPDATE - this is where the phrasemoji list of the day will be called */}
+            <h1 className="emojis">⭐️ ⭐️ ⭐️</h1>
+        </section>
+        <section>
+            <form>
+                <label>
+                    <input type="text" name="phrasemoji" placeholder="Answer" />
+                </label>
+                <br/>
+                <input type="submit" value="submit your phrasemoji" />
+            </form>
+        </section>
       </div>
     );
   }
